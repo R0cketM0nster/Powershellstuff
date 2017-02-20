04e6f0930aec1e0945d8688d995db9b366c95ce0
R0cketM0nster <daallen89@gmail.com>
2017-02-20 13:49:41 +1300

    Get-LoggedOnUser.ps1

---
+++ Get-LoggedOnUser.ps1
@@ -0,0 +1,1 @@
+function Get-LoggedOnUser{    [cmdletbinding()]    param    (        [parameter(Mandatory=$true)]        $ComputerName    )    Get-CimInstance -ClassName win32_computersystem -ComputerName $computername | select -ExpandProperty username -OutVariable a | Out-Null    $a.substring(10)}
