0a81ead5a804d68f89c6a561049e107da290e46f
R0cketM0nster <daallen89@gmail.com>
2017-02-22 13:44:13 +1300

    Formatting

--- Start-Gateway.ps1
+++ Start-Gateway.ps1
@@ -1,1 +1,10 @@
-$pc = Read-Host "PC name"$i = $pc.substring(0,6)if (Get-Process -ComputerName $pc -Name gateway -ErrorAction SilentlyContinue){    Stop-Process -Name gateway -Force    Start-Sleep -Seconds 5    Start-Process \\"$i"apps01\INCA_BIN\Gateway.exe }else {Start-Process \\"$i"apps01\INCA_BIN\Gateway.exe}
+$pc = Read-Host "PC name"
+$i = $pc.substring(0,6)
+if (Get-Process -ComputerName $pc -Name gateway -ErrorAction SilentlyContinue)
+{    
+    Stop-Process -Name gateway -Force    
+    Start-Sleep -Seconds 5    
+    Start-Process \\"$i"apps01\INCA_BIN\Gateway.exe 
+}
+else {Start-Process \\"$i"apps01\INCA_BIN\Gateway.exe}
+    
